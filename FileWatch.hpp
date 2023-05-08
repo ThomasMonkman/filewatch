8061c34b8b31328785980774f443b232c6feee7c
Thomas Monkman <thomas.monkman@holition.com>
2023-05-08 20:53:45 +0100

    fix: mingw

--- FileWatch.hpp
+++ FileWatch.hpp
@@ -756,7 +756,7 @@ namespace filewatch {
                               size / sizeof(TCHAR),
                               buf,
                               nullptr);
-                  return StringType{std::basic_string((C*)buf, length)};
+                  return StringType{std::basic_string<StringType>((C*)buf, length)};
             }
 #endif
 
