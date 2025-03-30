83af3f258cc7114c477b3150a750681222d572bd
Xichen Zhou <sichem.zh@gmail.com>
2025-03-30 11:35:10 -0400

    regex test
    Signed-off-by: Xichen Zhou <sichem.zh@gmail.com>

--- tests/Test.cpp
+++ tests/Test.cpp
@@ -144,28 +144,33 @@ TEST_CASE("copy assignment operator", "[operator]") {
 	REQUIRE(files_match);
 }
 
-// TEST_CASE("regex", "[regex]") {
-// 	const auto test_folder_path = testhelper::cross_platform_string("./");
-// 	const auto test_ignore_path = testhelper::cross_platform_string("./ignore.txt");
-// 	const auto test_file_name = testhelper::cross_platform_string("test.txt");
-// 	// create the file otherwise the Filewatch will throw
-// 	testhelper::create_and_modify_file(test_file_name);
+TEST_CASE("regex", "[regex]")
+{
+	const auto test_folder_path = testhelper::cross_platform_string("./");
+	const auto test_ignore_path = testhelper::cross_platform_string("./ignore.txt");
+	const auto test_file_name = testhelper::cross_platform_string("test.txt");
+	// create the file otherwise the Filewatch will throw
+	testhelper::create_and_modify_file(test_file_name);
 
-// 	std::promise<test_string> promise;
-// 	std::future<test_string> future = promise.get_future();
+	std::promise<test_string> promise;
+	std::future<test_string> future = promise.get_future();
 
-// 	filewatch::FileWatch<test_string> watch(test_folder_path, test_regex(testhelper::cross_platform_string("test.*")),[&promise, &test_file_name](const test_string& path, const filewatch::Event change_type) {
-// 		(void)change_type;
-// 		REQUIRE(path == test_file_name);
-// 		promise.set_value(path);
-// 	});
+	filewatch::FileWatch watch(
+	    test_folder_path,
+	    std::regex("test.*"),
+	    [&promise, &test_file_name](const test_string &path, const filewatch::Event change_type)
+	    {
+		    (void)change_type;
+		    REQUIRE(path == test_file_name);
+		    promise.set_value(path);
+	    });
 
-// 	testhelper::create_and_modify_file(test_ignore_path);
-// 	testhelper::create_and_modify_file(test_file_name);
+	testhelper::create_and_modify_file(test_ignore_path);
+	testhelper::create_and_modify_file(test_file_name);
 
-// 	auto path = testhelper::get_with_timeout(future);
-// 	REQUIRE(path == test_file_name);
-// }
+	auto path = testhelper::get_with_timeout(future);
+	REQUIRE(path == test_file_name);
+}
 
 #ifdef _WIN32
 //TEST_CASE("base type", "[char]") {
